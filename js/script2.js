a0d2dd5ca567eadcf78db3634971231192a5b646
YuriyKol <inter_worker@mail.ru>
2021-04-21 17:25:58 +0300

    SecondTestCommit

--- js/script2.js
+++ js/script2.js
@@ -1,3 +1,4 @@
+"use strict";
 //const result = confirm("Тут?")
 //const answer = prompt('Вам есть 18?','18 ' );
 //console.log(typeof(answer));
@@ -15,3 +16,18 @@ console.log(`https://someurl.com/${category}/5`);
 
 const user = "Ivan";
 alert(`Привет, ${user}`);
+
+console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+//incr++;
+//decr--;
+//++incr;
+//--decr;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(2*4 === 8);
