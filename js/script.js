02deac540fd4e3123ecfa7c9d59399d2375612cb
YuriyKol <inter_worker@mail.ru>
2021-04-21 17:22:52 +0300

    firstTestCommit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+console.log(1);
+let number = 5;
+const leftBorderLeft = 1;
+
+number = 10;
+console.log(number);
+
+console.log(4/0);
+console.log("string"*8);
+
+const person = "Alex";
+console.log(person);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
+
+let arr = ['plum.png', 'orange.jpg', 6, {}, []];
+console.log(arr[1]);
+
---
+++ js/script2.js
@@ -0,0 +1,17 @@
+//const result = confirm("Тут?")
+//const answer = prompt('Вам есть 18?','18 ' );
+//console.log(typeof(answer));
+
+//const answers = [];
+
+//answers[0] = prompt('Как Ваше имя?', '');
+//answers[1] = prompt('Как Ваша фамилия?', '');
+//answers[2] = prompt('Сколько Вам лет?', '');
+
+//document.write(answers);
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Ivan";
+alert(`Привет, ${user}`);
